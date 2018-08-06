b2ab3f1848fd5bbe10fc7b0d2885562250f5c5d9
Fitra Aditya <fitra@g.pl>
2018-08-06 08:45:29 +0700

    Enhanced pop3 server.

---
+++ pop3d/client.go
@@ -0,0 +1,213 @@
+package pop3d
+
+import (
+  "bufio"
+  "fmt"
+  "io"
+  "net"
+  "runtime"
+  "strconv"
+  "strings"
+  "sync"
+  "time"
+  "errors"
+
+  "github.com/fitraditya/surelin-smtpd/config"
+  "github.com/fitraditya/surelin-smtpd/data"
+  "github.com/fitraditya/surelin-smtpd/log"
+)
+
+const (
+  UNAUTHORIZED State = iota
+  TRANSACTION
+  UPDATE
+)
+
+type State int
+
+type Client struct {
+  server      *Server
+  state       State
+  remoteHost  string
+  time        int64
+  conn        net.Conn
+  bufin       *bufio.Reader
+  bufout      *bufio.Writer
+  kill_time   int64
+  id          int64
+  email       string
+}
+
+// Commands are dispatched to the appropriate handlers
+func (c *Client) handle(cmd string, args []string, line string) (ret bool) {
+  c.logTrace("In state %d, got command '%s', args '%s'", c.state, cmd, args)
+  c.logTrace(">" + cmd + "<")
+  arg, _ := c.parseArgs(args, 0)
+  c.logTrace("line>" + arg + "<")
+  c.logTrace(line)
+
+  if cmd == "USER" && c.state == UNAUTHORIZED {
+    c.tmp_client, _ = c.parseArgs(args, 0)
+
+    if c.server.Store.CheckUserExists(c.tmp_client) {
+      c.Write("+OK name is a valid mailbox")
+      c.logTrace(">+OK name is a valid mailbox")
+    } else {
+      c.Write("-ERR never heard of mailbox name " + c.tmp_client)
+      c.logTrace(">-ERR never heard of mailbox name " + c.tmp_client)
+    }
+
+    return false
+  } else if cmd == "PASS" && c.state == UNAUTHORIZED {
+    pass, _ := c.parseArgs(args, 0)
+
+    if c.server.Store.LoginUser(c.tmp_client, pass) {
+      c.Write("+OK mailbox ready")
+      c.logTrace(">+OK mailbox ready")
+      c.enterState(TRANSACTION)
+    } else {
+      c.Write("-ERR invalid password")
+      c.logTrace(">-ERR invalid password")
+    }
+
+    return false
+  } else if cmd == "STAT" && c.state == TRANSACTION {
+    nr_messages, size_messages := c.server.Store.StatMails(c.tmp_client)
+    c.Write("+OK " + strconv.Itoa(nr_messages) + " " + strconv.Itoa(size_messages))
+    c.logTrace(">+OK " + strconv.Itoa(nr_messages) + " " + strconv.Itoa(size_messages))
+  } else if cmd == "LIST" && c.state == TRANSACTION {
+    nr, tot_size, Message_head := c.server.Store.ListMails(c.tmp_client)
+    c.Write("+OK " + strconv.Itoa(nr) + " messages (" + strconv.Itoa(tot_size) + " octets)")
+    c.logTrace(">+OK " + strconv.Itoa(nr) + " messages (" + strconv.Itoa(tot_size) + " octets)")
+
+    // Print all messages
+    for _, val := range Message_head {
+      c.Write(strconv.Itoa(val.Id) + " " + strconv.Itoa(val.Size))
+    }
+
+    // Ending
+    c.Write(".")
+    return false
+  } else if cmd == "UIDL" && c.state == TRANSACTION {
+    nr, tot_size, Message_head := c.server.Store.ListMails(c.tmp_client)
+    c.Write("+OK " + strconv.Itoa(nr) + " messages (" + strconv.Itoa(tot_size) + " octets)")
+    c.logTrace(">+OK " + strconv.Itoa(nr) + " messages (" + strconv.Itoa(tot_size) + " octets)")
+
+    // Print all messages
+    for _, val := range Message_head {
+      c.Write(strconv.Itoa(val.Id) + " " + val.Uid)
+    }
+
+    // Ending
+    c.Write(".")
+    return false
+  } else if cmd == "RETR" && c.state == TRANSACTION  {
+    id, _ := c.parseArgs(args, 0)
+    i, _ := strconv.Atoi(id)
+    // Retreive one message but don't delete it from the server
+    message, size := c.server.Store.GetMail(c.tmp_client, i)
+    c.Write("+OK " + strconv.Itoa(size) + " octets")
+    c.Write(message.Content.Body)
+    // Ending
+    c.Write(".")
+    return false
+  } else if cmd == "DELE" && c.state == TRANSACTION  {
+    id, _ := c.parseArgs(args, 0)
+    i, _ := strconv.Atoi(id)
+    c.server.Store
+    c.Write("+OK message deleted")
+    c.logTrace(">+OK message deleted")
+    return false
+  } else if cmd == "TOP" && c.state == TRANSACTION {
+    arg, _ := c.parseArgs(args, 0)
+    nr, _ := strconv.Atoi(arg)
+    headers := c.server.Store.TopMail(c.tmp_client, nr)
+    c.Write("+OK top message follows")
+    c.Write(headers + "\r\n\r\n.")
+    return false
+  } else if cmd == "CAPA" {
+    c.Write("TOP")
+    c.Write("UIDL")
+    // Ending
+    c.Write(".")
+    return false
+  } else if cmd == "QUIT" {
+    return true
+  } else {
+    c.Write("-ERR not implemented")
+    return false
+  }
+
+  return false
+}
+
+func (c *Client) enterState(state State) {
+  c.state = state
+  c.logInfo("Entering state %v", state)
+}
+
+func (c *Client) greet() {
+  c.Write(fmt.Sprintf("+OK %v Surelin POP3 # %s (%s) %s", c.server.domain, strconv.FormatInt(c.id, 10), strconv.Itoa(len(c.server.sem)), time.Now().Format(time.RFC1123Z)))
+  c.enterState(UNAUTHORIZED)
+}
+
+// Calculate the next read or write deadline based on maxIdleSeconds
+func (c *Client) nextDeadline() time.Time {
+  return time.Now().Add(time.Duration(c.server.maxIdleSeconds) * time.Second)
+}
+
+func (c *Client) Write(text string) {
+  c.conn.SetDeadline(c.nextDeadline())
+  c.logTrace(">> Sent %d bytes: %s >>", len(text), text)
+  c.conn.Write([]byte(text + "\r\n"))
+  c.bufout.Flush()
+}
+
+// Reads a line of input
+func (c *Client) readLine() (line string, err error) {
+  if err = c.conn.SetReadDeadline(c.nextDeadline()); err != nil {
+    return "", err
+  }
+
+  line, err = c.bufin.ReadString('\n')
+
+  if err != nil {
+    return "", err
+  }
+
+  c.logTrace("<< %v <<", strings.TrimRight(line, "\r\n"))
+  return line, nil
+}
+
+func (c *Client) parseCmd(line string) (cmd string, arg []string) {
+  line = strings.Trim(line, "\r \n")
+  cm := strings.Split(line, " ")
+  return strings.ToUpper((cm[0])[0:4]), cm[1:]
+}
+
+func (c *Client) parseArgs(args []string, nr int) (arg string, err error) {
+  if nr < len(args) {
+    return args[nr], nil
+  }
+
+  return "", errors.New("Out of range")
+}
+
+// Session specific logging methods
+func (c *Client) logTrace(msg string, args ...interface{}) {
+  log.LogTrace("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
+}
+
+func (c *Client) logInfo(msg string, args ...interface{}) {
+  log.LogInfo("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
+}
+
+func (c *Client) logWarn(msg string, args ...interface{}) {
+  // Update metrics
+  log.LogWarn("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
+}
+
+func (c *Client) logError(msg string, args ...interface{}) {
+  // Update metrics
+  log.LogError("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
+}
--- pop3d/server.go
+++ pop3d/server.go
@@ -17,14 +17,6 @@ import (
   "github.com/fitraditya/surelin-smtpd/log"
 )
 
-const (
-  STATE_UNAUTHORIZED = 1
-  STATE_TRANSACTION = 2
-  STATE_UPDATE = 3
-)
-
-type State int
-
 var commands = map[string]bool{
   "TOP":      true,
   "USER":     true,
@@ -37,7 +29,6 @@ var commands = map[string]bool{
   "QUIT":     true,
 }
 
-// Real server code starts here
 type Server struct {
   Store           *data.DataStore
   domain          string
@@ -50,20 +41,7 @@ type Server struct {
   sem             chan int
 }
 
-type Client struct {
-  server      *Server
-  state       State
-  remoteHost  string
-  time        int64
-  conn         net.Conn
-  bufin       *bufio.Reader
-  bufout      *bufio.Writer
-  kill_time   int64
-  id          int64
-  tmp_client  string
-}
-
-// Init a new Client object
+// Init a new Server object
 func NewPop3Server(cfg config.Pop3Config, ds *data.DataStore) *Server {
   // sem is an active clients channel used for counting clients
   maxClients := make(chan int, cfg.MaxClients)
@@ -86,7 +64,6 @@ func (s *Server) Start() {
   if err != nil {
     log.LogError("Failed to build tcp4 address: %v", err)
     // TODO More graceful early-shutdown procedure
-    //panic(err)
     s.Stop()
     return
   }
@@ -98,7 +75,6 @@ func (s *Server) Start() {
   if err != nil {
     log.LogError("POP3 failed to start tcp4 listener: %v", err)
     // TODO More graceful early-shutdown procedure
-    //panic(err)
     s.Stop()
     return
   }
@@ -130,8 +106,7 @@ func (s *Server) Start() {
           return
         }
 
-        // TODO Implement a max error counter before shutdown?
-        // or maybe attempt to restart POP3d
+        // TODO Implement a max error counter before shutdown
         panic(err)
       }
     } else {
@@ -221,175 +196,4 @@ func (s *Server) handleClient(c *Client) {
   }
 
   c.logInfo("Closing connection")
-}
-
-// Commands are dispatched to the appropriate handlers.
-func (c *Client) handle(cmd string, args []string, line string) (ret bool) {
-  c.logTrace("In state %d, got command '%s', args '%s'", c.state, cmd, args)
-
-  c.logTrace(">" + cmd + "<")
-  arg, _ := c.parseArgs(args, 0)
-  c.logTrace("line>" + arg + "<")
-  c.logTrace(line)
-
-  if cmd == "USER" && c.state == STATE_UNAUTHORIZED {
-    c.tmp_client, _ = c.parseArgs(args, 0)
-
-    if c.server.Store.CheckUserExists(c.tmp_client) {
-      c.Write("+OK name is a valid mailbox")
-      c.logTrace(">+OK name is a valid mailbox")
-    } else {
-      c.Write("-ERR never heard of mailbox name " + c.tmp_client)
-      c.logTrace(">-ERR never heard of mailbox name " + c.tmp_client)
-    }
-
-    return false
-  } else if cmd == "PASS" && c.state == STATE_UNAUTHORIZED {
-    pass, _ := c.parseArgs(args, 0)
-
-    if c.server.Store.LoginUser(c.tmp_client, pass) {
-      c.Write("+OK mailbox ready")
-      c.logTrace(">+OK mailbox ready")
-      c.state = 2
-    } else {
-      c.Write("-ERR invalid password")
-      c.logTrace(">-ERR invalid password")
-    }
-
-    return false
-  } else if cmd == "STAT" && c.state == STATE_TRANSACTION {
-    nr_messages, size_messages := c.server.Store.StatMails(c.tmp_client)
-    c.Write("+OK " + strconv.Itoa(nr_messages) + " " + strconv.Itoa(size_messages))
-    c.logTrace(">+OK " + strconv.Itoa(nr_messages) + " " + strconv.Itoa(size_messages))
-  } else if cmd == "LIST" && c.state == STATE_TRANSACTION {
-    c.logTrace("List accepted")
-    nr, tot_size, Message_head := c.server.Store.ListMails(c.tmp_client)
-    c.Write("+OK " + strconv.Itoa(nr) + " messages (" + strconv.Itoa(tot_size) + " octets)")
-
-    // Print all messages
-    for _, val := range Message_head {
-      c.Write(strconv.Itoa(val.Id) + " " + strconv.Itoa(val.Size))
-    }
-
-    // Ending
-    c.Write(".")
-    return false
-  } else if cmd == "UIDL" && c.state == STATE_TRANSACTION {
-    c.logTrace("Uidl accepted")
-    nr, tot_size, Message_head := c.server.Store.ListMails(c.tmp_client)
-    c.Write("+OK " + strconv.Itoa(nr) + " messages (" + strconv.Itoa(tot_size) + " octets)")
-
-    // Print all messages
-    for _, val := range Message_head {
-      c.Write(strconv.Itoa(val.Id) + " " + val.Uid)
-    }
-
-    // Ending
-    c.Write(".")
-    return false
-  } else if cmd == "RETR" && c.state == STATE_TRANSACTION  {
-    id, _ := c.parseArgs(args, 0)
-    i, _ := strconv.Atoi(id)
-    // Retreive one message but don't delete it from the server
-    message, size := c.server.Store.GetMail(c.tmp_client, i)
-    c.Write("+OK " + strconv.Itoa(size) + " octets")
-    c.Write(message.Content.Body)
-    // Ending
-    c.Write(".")
-    return false
-  } else if cmd == "DELE" && c.state == STATE_TRANSACTION  {
-    //id, _ := c.parseArgs(args, 0)
-    //i, _ := strconv.Atoi(id)
-    // Dummy delete from the server
-    c.Write("+OK message deleted")
-    return false
-  } else if cmd == "TOP" && c.state == STATE_TRANSACTION {
-    arg, _ := c.parseArgs(args, 0)
-    nr, _ := strconv.Atoi(arg)
-    headers := c.server.Store.TopMail(c.tmp_client, nr)
-    c.Write("+OK top message follows")
-    c.Write(headers + "\r\n\r\n.")
-    return false
-  } else if cmd == "CAPA" {
-    c.Write("TOP")
-    c.Write("UIDL")
-    // Ending
-    c.Write(".")
-    return false
-  } else if cmd == "QUIT" {
-    return true
-  } else {
-    c.Write("-ERR not implemented")
-    return false
-  }
-
-  return false
-}
-
-func (c *Client) greet() {
-  c.Write(fmt.Sprintf("+OK %v Surelin POP3 # %s (%s) %s", c.server.domain, strconv.FormatInt(c.id, 10), strconv.Itoa(len(c.server.sem)), time.Now().Format(time.RFC1123Z)))
-  c.state = 1
-}
-
-// Calculate the next read or write deadline based on maxIdleSeconds
-func (c *Client) nextDeadline() time.Time {
-  return time.Now().Add(time.Duration(c.server.maxIdleSeconds) * time.Second)
-}
-
-func (c *Client) Write(text string) {
-  c.conn.SetDeadline(c.nextDeadline())
-  c.logTrace(">> Sent %d bytes: %s >>", len(text), text)
-  c.conn.Write([]byte(text + "\r\n"))
-  c.bufout.Flush()
-}
-
-// Reads a line of input
-func (c *Client) readLine() (line string, err error) {
-  if err = c.conn.SetReadDeadline(c.nextDeadline()); err != nil {
-    return "", err
-  }
-
-  line, err = c.bufin.ReadString('\n')
-
-  if err != nil {
-    return "", err
-  }
-
-  c.logTrace("<< %v <<", strings.TrimRight(line, "\r\n"))
-  return line, nil
-}
-
-func (c *Client) parseCmd(line string) (cmd string, arg []string) {
-  line = strings.Trim(line, "\r \n")
-  cm := strings.Split(line, " ")
-  return strings.ToUpper((cm[0])[0:4]), cm[1:]
-}
-
-func (c *Client) parseArgs(args []string, nr int) (arg string, err error) {
-  if nr < len(args) {
-    return args[nr], nil
-  }
-
-  return "", errors.New("Out of range")
-}
-
-// Session specific logging methods
-func (c *Client) logTrace(msg string, args ...interface{}) {
-  log.LogTrace("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
-}
-
-func (c *Client) logInfo(msg string, args ...interface{}) {
-  log.LogInfo("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
-}
-
-func (c *Client) logWarn(msg string, args ...interface{}) {
-  // Update metrics
-  //expWarnsTotal.Add(1)
-  log.LogWarn("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
-}
-
-func (c *Client) logError(msg string, args ...interface{}) {
-  // Update metrics
-  //expErrorsTotal.Add(1)
-  log.LogError("POP3[%v]<%v> %v", c.remoteHost, c.id, fmt.Sprintf(msg, args...))
 }
